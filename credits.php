fe86c97f6c3e32d94b77b103deea957926f36947
krzyszp <krzyszp@interia.pl>
2017-02-28 13:00:34 +0100

    Update credits.php

--- credits.php
+++ credits.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of DrugDiscovery@Home  project.
+// This file is part of DrugDiscovery@Home BOINC project.
 // http://boinc.drugdiscoveryathome.com
 // Copyright (C) 2017 by Krzysztof 'krzyszp' Piszczek
 // This file is available under GNU GPL3 licence
@@ -15,12 +15,12 @@ if ($conn->connect_error) {
 $appid = mysqli_real_escape_string($conn, $_GET['appid']);
 $key = $_GET['key'];
 
-if ($key == 'XXXXXXXXX') {
+if ($key == 'XXXXXXX') { 
     if (! filter_var($appid, FILTER_VALIDATE_INT)) {
         echo "Your variable is not an integer";
     }else {
-        //$sql = "SELECT id, workunitid, userid, hostid, received_time, name, flops_estimate, granted_credit, address FROM result RIGHT JOIN eth_wallets ON result.userid = eth_wallets.user_id left join eth_payments on result.id = eth_payments.taskid WHERE result.appid = $appid AND server_state = 5 AND validate_state = 1 AND eth_payments.status <> 0";
-    	$sql = "SELECT id, workunitid, userid, hostid, received_time, name, flops_estimate, granted_credit, address FROM result RIGHT JOIN eth_wallets ON result.userid = eth_wallets.user_id WHERE result.appid = $appid AND server_state = 5 AND validate_state = 1 and result.id not in (select taskid from eth_payments where eth_payments.status != 0) limit 10000";
+    	header("Access-Control-Allow-Origin: *");
+    	$sql = "SELECT id, workunitid, userid, hostid, received_time, name, flops_estimate, granted_credit, address FROM result RIGHT JOIN eth_wallets ON result.userid = eth_wallets.user_id WHERE result.appid = $appid AND server_state = 5 AND validate_state = 1 and result.id not in (select taskid from eth_payments where eth_payments.status != 0) limit 1000";
         $result = $conn->query($sql);
         if ($result->num_rows > 0) {
             $rows = array();
